3cfb7fa2307bb32f602c2a945a6c0cc656f10f3a
cameron.hawkins10 <cameron.hawkins10@gmail.com>
2024-04-02 22:20:54 -0400

    update quiz content and correct answers

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const correctAnswers = ['B', 'B', 'B', 'B'];
+const correctAnswers = ['B', 'B', 'A', 'A'];
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
 
